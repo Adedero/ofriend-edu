6c84acd98f2ea128cfa97affe13dc97d7bfef686
Adedero <118962395+Adedero@users.noreply.github.com>
2024-11-22 08:41:04 +0100

    fb

--- server/src/config/firebase-admin.config.ts
+++ server/src/config/firebase-admin.config.ts
@@ -4,13 +4,13 @@ import { getStorage } from 'firebase-admin/storage';
 //import serviceAccount from '../../secrets/ofriend-6ce1c-firebase-adminsdk-fl83g-255faa2a44.json';
 
 const pathToServiceAccount = path.resolve('secrets', 'ofriend-6ce1c-firebase-adminsdk-fl83g-255faa2a44.json');
-(async function() {
+const initializeApp = async () => {
   const serviceAccount = await import(pathToServiceAccount);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as ServiceAccount),
     storageBucket: 'gs://ofriend-6ce1c.appspot.com'
   });
-})();
+})p
 
 const bucket = getStorage().bucket();
 
